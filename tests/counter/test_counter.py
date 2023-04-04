ec311ca86c5f875b8c0cf2a1f5ec20af185b96d5
GustavoDimeira <gustavodimeira1@gmail.com>
2023-04-04 08:33:35 -0400

    fix: tests

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,5 +2,5 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    counter = count_ocurrences("tests/counter/fileToRead.txt", "ipsum")
-    assert counter == 7
+    counter = count_ocurrences("data/jobs.csv", "file")
+    assert counter == 306
